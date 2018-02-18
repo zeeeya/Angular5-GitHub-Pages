7cd861f25548c81669cbc7feab52677597ed9830
zeeeya <zci.alvarez@gmail.com>
2018-02-19 01:26:15 +0800

    Routing

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -6,8 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private route: ActivatedRoute, private router: Router) { 
+    this.route.params.subscribe(res => console.log(res.id));
+  }
 
   ngOnInit() {
   }
