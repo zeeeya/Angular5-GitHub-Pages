30353c10c86ac76f6acc81cc03373376620aa421
zeeeya <zci.alvarez@gmail.com>
2018-02-19 00:56:59 +0800

    Templating and Styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styles: [`
+  
+  p { font-weight: bold; }
+  div { color: gray; }
+  
+  `]
 })
 export class HomeComponent implements OnInit {
 
